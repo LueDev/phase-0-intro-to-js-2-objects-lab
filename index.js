ad3040fee94f7923a3b05eed545594afb3312f3e
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-08-18 13:15:29 -0400

    All tests are passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name : "defaultName",
+    streetAddress : "defaultAddress"
+};
+
+function updateEmployeeWithKeyAndValue(emp, key, value){
+    const newEmployee = {...emp};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value){
+    emp[key] = value;
+    return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key){
+    const newEmployee = {...emp};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key){
+    delete emp[key];  //destructive way of deleting from object.
+    return emp;
+}
